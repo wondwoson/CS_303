1cf28f6587b71c515aea91c1301870e446e1ba8f
wondwoson <39657205+wondwoson@users.noreply.github.com>
2021-03-23 23:34:58 -0500

    Add files via upload

--- labW1D2/app.js
+++ labW1D2/app.js
@@ -1,4 +1,22 @@
-function load() {
-	let id = document.getElementById("result");
-	id.innerHTML = "Lab W1D2";
+/* eslint-disable require-jsdoc */
+/* eslint-disable no-unused-vars */
+"use strict";
+
+// See tests.js for behavior
+function nameString(obj) {
+     if(obj===undefined){
+         return "no object passed"
+     }else if(obj===null){
+         return "null argument";
+     }
+     else {
+         if(Object.keys(obj).length===0){
+        return "object has no properties"
+     }
+     else {
+       return Object.keys(obj).sort().join(".");
+     }
+ }
 }
+
+
