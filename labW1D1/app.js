a5d421e9d628724b3b659aec325578116eee1dba
wondwoson <wongotera2014@gmail.com>
2021-03-26 00:33:49 -0500

    OOP1

--- labW1D1/app.js
+++ labW1D1/app.js
@@ -11,17 +11,18 @@ function add2(n1, n2) {
 }
 
 function add(...args) {
-	let count = arguments.length;
+	console.log(args)
+	let count = args.length;
 	let sum = 0;
 	for (let i=0; i<count; i++) {
-		sum += arguments[i];		
+		sum += args[i];		
 	}
 	
 	return sum;
 }
 
 /*
-	Input: grades is an array of integers
+	Input: grades is an array of integers = [n, n, ...]
 	Output: compute average of grades and report letter grade as follows:
 	90 - 100 A
 	80 - 89  B
@@ -30,6 +31,26 @@ function add(...args) {
 	0 - 59   NC
 */
 function computeGrade(grades) {
-	return "???";
+	let sum=0;
+	for(let i=0;i<grades.length;i++){
+		sum+=grades[i];
+	}
+	let average = sum/grades.length;
+   
+	let letter="";
+	if(average>=90){
+		letter="A";
+	}else if(average>=80){
+		letter= "B";
+	}else if (average>=70){
+		letter="C";
+	}else if(average>=60){
+		letter="D";
+	}else{
+		letter="NC";
+	}
+		
+	return letter;
 }
+//console.log(computeGrade([80,50,80]));
 
