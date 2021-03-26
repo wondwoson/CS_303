62689eee5701afda2797d56dc252583aacb1f969
wondwoson <wongotera2014@gmail.com>
2021-03-26 00:23:30 -0500

    OOP4

--- labW1D4/app.js
+++ labW1D4/app.js
@@ -1,4 +1,88 @@
-function load() {
-	let id = document.getElementById("result");
-	id.innerHTML = "Lab W1D4";
+/* eslint-disable require-jsdoc */
+/* eslint-disable no-unused-vars */
+"use strict";
+
+// Remove the even characters from string, e.g removeEvenCharacters("abcd") === "bd"
+function removeEvenCharacters(string) {
+    if(string===undefined){
+        return "missing argument"
+    }
+
+    if(typeof string != "string"){
+        return "argument not string";
+    }
+
+    if(string===""){
+        return "";
+    }
+
+    let strArr=string.split("");
+    string="";
+    for(let i=1; i<strArr.length; i+=2){
+        string+=strArr[i];
+    }
+
+    return string;
 }
+
+
+
+// Sum the numbers that are greater than zero of array, e.g. sumPositiveNumbers([-1,4,5,-2,-3,10]) === 19
+// Must use the reduce method of the Array object to do this!!!
+function sumPositiveNumbers(array) {
+   /* return array.reduce((sum, n)=> {
+        sum = (sum > 0) ? sum : 0;
+        n = (n > 0) ? n : 0;
+        return n + sum;
+    });
+    */
+
+    let filtered = array.filter(n => n > 0);
+    if (filtered.length < 1) {
+        return 0;
+    }
+
+    return filtered.reduce((acc, n) => acc + n);
+
+}
+
+// Write a construction function
+// Input an array containing numbers and string, e.g. [1, "a", "c", 2, 4]
+// Creates an object with two array properties numbers and strings, e.g.
+// new ArrayDemo([1, "a", "c", 2, 4]) returns {numbers:[1, 2, 4]; strings:["a", "c"}
+function ArrayDemo(array) {
+    return {
+        numbers: array.filter(x => typeof x == 'number'),
+        strings: array.filter(x => typeof x == 'string')
+    };
+}
+
+// Makes all negative numbers of the input array positive
+function makePositive(array) {
+    return array.map(x => (typeof x === 'number') ? Math.abs(x) : x);
+}
+
+// Replaces sequences of 0s with an "*", e.g.
+// removeZeros([3, 0, 0, 0, 5, 6, 0, 0, 7, 0]) returns [3, "*", 5, "*", 7, "*"];
+function removeZeros(array) {
+    let filtered = [];
+    let flag = false;
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] == 0) {
+            if (!flag) {
+                array[i] = '*';
+                flag = true;
+            } else {
+                continue;
+            }
+        } else {
+            flag = false;
+        }
+        filtered.push(array[i]);
+    }
+    return filtered; 
+    //'12*5'
+}
+console.log(removeZeros([1,2,0, 0, 0,5,0,4,0,0,9,7]))
+
+
--- labW1D4/test.js
+++ labW1D4/test.js
@@ -1,1 +1,97 @@
-/* mocha tests */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+/* eslint-disable require-jsdoc */
+"use strict";
+
+function mochaTests() {
+    describe("removeEvenCharacters", function () {
+        it("non-empty string", function () {
+            assert.equal(removeEvenCharacters("abcdefgh"), "bdfh");
+        });
+	});
+	
+    describe("removeEvenCharacters", function () {
+        it("empty string", function () {
+            assert.equal(removeEvenCharacters(""), "");
+        });
+	});
+	
+    describe("removeEvenCharacters", function () {
+        it("missing argument", function () {
+            assert.equal(removeEvenCharacters(), "missing argument");
+        });
+	});
+	
+    describe("removeEvenCharacters", function () {
+        it("argument not string", function () {
+            assert.equal(removeEvenCharacters(5), "argument not string");
+        });
+	});	
+
+    describe("sumPositiveNumbers", function () {
+        it("mixed", function () {
+            assert.equal(sumPositiveNumbers([-1, 4, 5, -6, 10]), 19);
+        });
+	});	
+
+    describe("sumPositiveNumbers", function () {
+        it("all positive", function () {
+            assert.equal(sumPositiveNumbers([1, 4, 5, 6, 10]), 26);
+        });
+	});	
+	
+    describe("sumPositiveNumbers", function () {
+        it("all negative", function () {
+            assert.equal(sumPositiveNumbers([-1, -4, -5, -6, -10]), 0);
+        });
+	});	
+	
+    describe("sumPositiveNumbers", function () {
+        it("empty array", function () {
+            assert.equal(sumPositiveNumbers([]), 0);
+        });
+	});	
+
+    describe("ArrayDemo", function () {
+        it("mixed", function () {
+			let result = new ArrayDemo([1, "a", 2, "b", 3]).numbers.join(".");
+            assert.equal(result, "1.2.3");
+			
+			result = new ArrayDemo([1, "a", 2, "b", 3]).strings.join(".");
+            assert.equal(result, "a.b");
+        });
+	});	
+
+    describe("ArrayDemo", function () {
+        it("empty", function () {
+			let result = new ArrayDemo([]).numbers.join(".");
+            assert.equal(result, "");
+			
+			result = new ArrayDemo([]).strings.join(".");
+            assert.equal(result, "");
+        });
+	});	
+
+   describe("makePositive", function () {
+        it("some negatives", function () {
+			let result = makePositive([-1, 2, "a", 3, -4, -5]).join(".");
+            assert.equal(result, "1.2.a.3.4.5");			
+        });
+	});	
+
+   describe("makePositive", function () {
+        it("empty array", function () {
+			let result = makePositive([]).join(".");
+            assert.equal(result, "");			
+        });
+	});	
+	
+  describe("removeZeros", function () {
+        it("has zeros", function () {
+			let result = removeZeros([5, 0, 0, 0, 6, 0,0,5, 0]).join(".");
+            assert.equal(result, "5.*.6.*.5.*");			
+        });
+	});	
+		
+
+}
