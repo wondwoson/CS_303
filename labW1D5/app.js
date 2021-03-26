b498c4d39b1526a3738125a5bb48c65abe0c85df
wondwoson <wongotera2014@gmail.com>
2021-03-26 15:09:36 -0500

    OOP5

--- labW1D5/app.js
+++ labW1D5/app.js
@@ -1,4 +1,45 @@
-function load() {
-	let id = document.getElementById("result");
-	id.innerHTML = "Lab W1D5";
+/* eslint-disable require-jsdoc */
+/* eslint-disable no-unused-vars */
+"use strict";
+
+function countSubstring(s, target) {
 }
+
+// Save obj in localStorage
+function saveInLocalStorage(name, obj) {
+}
+
+// Return value of name in local storage
+function getFromLocalStorage(name) {
+}
+
+// This function has an unbounded this. When it is called, the this will be bound to some object
+// The argument "obj" is a single key/value pair, e.g. {location:"Fairfield"}
+// It can be any key/value pair
+// addMissProperty tests the object bound to its "this" for the key of "obj" (e.g., location)
+// If the bound object doesn't have that key then the key and its value is added to the bound object.
+function addMissingProperty(obj) {
+}
+
+// Constructor function which creates object {name:name, country:country, creationData:...}
+// Where creationDate is the time that the object is created.
+function Person(name, country) {
+}
+
+
+// The parameter "students" is an array of student objects. Each student object has form
+// {name:some name, country: some country}
+// This function creates a Map using the "students" parameter and sets the value of each student
+// in it to zero. This will be the number of classes that the student has missed.
+// The function incrementAbscences (see next) can be called to increment the number of absences.
+function register(students) {
+}
+
+// Parameters:
+// course is a Map mapping student objects to an integer which is the number of days missed
+// student is an object that can be used as a key to course.
+// This function increments the number of days missed for the student.
+function incrementAbsences(course, student) {
+}
+
+
--- labW1D5/test.js
+++ labW1D5/test.js
@@ -1,1 +1,83 @@
-/* mocha tests */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+/* eslint-disable require-jsdoc */
+"use strict";
+
+function mochaTests() {
+    describe("countSubstring", function () {
+        it("substring present", function () {
+			let result = countSubstring("5abdxyxabdabd", "abd");
+            assert.equal(result, 3);
+        });
+	});		
+
+    describe("countSubstring", function () {
+        it("substring not present", function () {
+			let result = countSubstring("5abdxyxabdabd", "xxx");
+            assert.equal(result, 0);
+        });
+	});		
+	
+    describe("saveInLocalStorage", function () {
+        it("save", function () {
+			saveInLocalStorage("CS303test", {name:"bob", country:"usa", grades:[80,90]});
+			let result = JSON.parse(localStorage.getItem("CS303test"));
+            assert.equal(result.name, "bob");
+        });
+	});		
+    
+	describe("getFromLocalStorage", function () {
+        it("get", function () {
+			let result = getFromLocalStorage("CS303test");
+            assert.equal(result.country, "usa");
+        });
+	});		
+
+	describe("addMissingProperties", function () {
+        it("location not present", function () {
+			let obj = {name:"bob", school:"miu"};
+			obj.f = addMissingProperty;
+			obj.f({location:"fairfield"});
+            assert.equal(obj.location, "fairfield");
+        });
+	});	
+
+	describe("addMissingProperties", function () {
+        it("location present", function () {
+			let obj = {name:"bob", school:"miu", location:"india"};
+			obj.f = addMissingProperty;
+			obj.f({location:"fairfield"});
+            assert.equal(obj.location, "india");
+        });
+	});			
+	
+	describe("Person", function () {
+        it("date", function () {
+			let person = new Person("bob", "usa");
+			let date = new Date();
+            assert.equal(person.creationDate.getFullYear(), 2021);
+			assert.equal(person.creationDate.getMonth(), 2);
+        });
+	});			
+
+	describe("Register", function () {
+        it("assign", function () {
+			let students = [{name:"bob", country:"usa"}, {name:"jill", country:"usa"}];
+			let course = register(students);
+			assert.equal(course.get(students[0]), 0);
+			assert.equal(course.get(students[1]), 0);
+        });
+	});			
+
+	describe("incrementAbsences", function () {
+        it("mark absent", function () {
+			let students = [{name:"bob", country:"usa"}, {name:"jill", country:"usa"}];
+			let course = register(students);
+			let student = students[0];
+			incrementAbsences(course, student);
+			incrementAbsences(course, student);
+			incrementAbsences(course, student);
+			assert.equal(course.get(student), 3);
+        });
+	});			
+}
